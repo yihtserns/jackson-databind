f8b9f19e07330f274c622aca000e5f9eb54bf2ab
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-10-28 20:14:20 -0700

    Last touches...

--- src/main/java/com/fasterxml/jackson/databind/ser/SerializerCreationContext.java
+++ src/main/java/com/fasterxml/jackson/databind/ser/SerializerCreationContext.java
@@ -18,29 +18,37 @@ public interface SerializerCreationContext {
     public AnnotatedClass classInfo();
 
     public static class Simple implements SerializerCreationContext {
-        private final SerializationConfig _config;
-        private final BeanDescription _desc;
+        private final SerializerProvider _ctxt;
+        private final JavaType _type;
 
-        public Simple(SerializationConfig config, BeanDescription desc) {
-            _config = config;
-            _desc = desc;
+        private BeanDescription _desc;
+
+        public Simple(SerializerProvider ctxt, JavaType type) {
+            _ctxt = ctxt;
+            _type = type;
         }
 
         public static Simple from(SerializerProvider prov, JavaType forType) {
-            return new Simple(prov.getConfig(),
-                    prov.introspectBeanDescription(forType));
+            return new Simple(prov, forType);
         }
         
         @Override
-        public SerializationConfig config() { return _config; }
+        public SerializationConfig config() { return _ctxt.getConfig(); }
 
         @Override
-        public AnnotationIntrospector annotationIntrospector() { return _config.getAnnotationIntrospector(); }
+        public AnnotationIntrospector annotationIntrospector() { return _ctxt.getAnnotationIntrospector(); }
         
         @Override
-        public BeanDescription beanDescription() { return _desc; }
+        public BeanDescription beanDescription() {
+            if (_desc == null) {
+                _desc = _ctxt.introspectBeanDescription(_type);
+            }
+            return _desc;
+        }
 
         @Override
-        public AnnotatedClass classInfo() { return _desc.getClassInfo(); }
+        public AnnotatedClass classInfo() {
+            return beanDescription().getClassInfo();
+        }
     }
 }
